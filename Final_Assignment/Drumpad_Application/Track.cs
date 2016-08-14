b69767b205244b7a457869a36a65604007248bf7
DENYS\Denys <denisic96@yandex.ua>
2016-08-14 01:40:45 -0400

    Yopta

---
+++ Final_Assignment/Drumpad_Application/Track.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drumpad_Application
+{
+    class Track
+    {
+    }
+}
