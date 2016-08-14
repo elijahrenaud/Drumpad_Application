8af2086e3dda1e3eef08ef92e0289e199c39d9cb
DENYS\Denys <denisic96@yandex.ua>
2016-08-14 03:13:38 -0400

    Write

--- Final_Assignment/Drumpad_Application/4Button.Designer.cs
+++ Final_Assignment/Drumpad_Application/4Button.Designer.cs
@@ -32,10 +32,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -47,6 +47,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Pad1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -56,6 +57,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Pad2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -65,6 +67,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Pad3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -74,42 +77,47 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Pad4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // btnPlay
             // 
-            this.button5.Location = new System.Drawing.Point(359, 130);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(104, 45);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Play";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnPlay.Location = new System.Drawing.Point(359, 130);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(104, 45);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
-            // button6
+            // btnPause
             // 
-            this.button6.Location = new System.Drawing.Point(359, 200);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(104, 45);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Pause";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnPause.Location = new System.Drawing.Point(359, 200);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(104, 45);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
-            // button7
+            // btnStop
             // 
-            this.button7.Location = new System.Drawing.Point(359, 288);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(104, 45);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "Stop";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnStop.Location = new System.Drawing.Point(359, 288);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(104, 45);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
-            // button8
+            // btnRecord
             // 
-            this.button8.Location = new System.Drawing.Point(359, 358);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(104, 45);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "Record";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btnRecord.Location = new System.Drawing.Point(359, 358);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(104, 45);
+            this.btnRecord.TabIndex = 7;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
             // 
             // textBox1
             // 
@@ -125,10 +133,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(493, 434);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -146,10 +154,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRecord;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Final_Assignment/Drumpad_Application/4Button.cs
+++ Final_Assignment/Drumpad_Application/4Button.cs
@@ -12,9 +12,52 @@ namespace Drumpad_Application
 {
     public partial class Form1 : Form
     {
+        Player p;
         public Form1()
         {
             InitializeComponent();
+            p = new Player();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p.push(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            p.push(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            p.push(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            p.push(4);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+             foreach (string s in p.song)
+                textBox1.Text += s;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            p.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            p.stop();
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            p.start();
         }
     }
 }
