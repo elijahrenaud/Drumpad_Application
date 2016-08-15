c88111a51ca94061cad2e61685af009217fbd7b0
DENYS\Denys <denisic96@yandex.ua>
2016-08-15 01:14:23 -0400

    Menu

--- Final_Assignment/Drumpad_Application/FourButton.cs
+++ Final_Assignment/Drumpad_Application/FourButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,7 +48,7 @@ namespace Drumpad_Application
 
         private void btnPlay_Click(object sender, EventArgs e)
         {
-            foreach (string s in p.song)
+            foreach (char s in p.song)
                 textBox1.Text += s;
         }
 
@@ -64,6 +65,39 @@ namespace Drumpad_Application
         private void btnRecord_Click(object sender, EventArgs e)
         {
             p.start();
+        }
+
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofdImport.Title = "Open Song File";
+            ofdImport.Filter = "txt song files|*.txt";
+            if (ofdImport.ShowDialog() == DialogResult.OK) {
+                string data = File.ReadAllText(ofdImport.FileName);
+                foreach (char c in data) {
+                    if (!(c.Equals('-') || char.IsDigit(c)))
+                    {
+                        MessageBox.Show("This file is not a song. Please try other file");
+                        return;
+                    }
+                    else {
+                        p = new Player(data);
+                    }
+                } 
+            }
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sfdexport.Title = "Save as text file";
+            sfdexport.Filter = "txt song files|*.txt";
+            if (sfdexport.ShowDialog() == DialogResult.OK) {
+                File.WriteAllText(sfdexport.FileName, p.song);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
